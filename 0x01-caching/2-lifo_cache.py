5ed1df6c4de5e40d9d8f7f4c2f8eab3769f9ae05
elayneh <betenatefera@gmail.com>
2022-07-13 15:34:10 +0300

    little change

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -21,7 +21,7 @@ class LIFOCache(BaseCaching):
 
     def get(self, key):
         """ Returns linked value """
-        if key is None or self.cache_data[key] is None:
+        if key is None or self.cache_data.get(key) is None:
             return None
         if key in self.cache_data:
             return self.cache_data[key]
