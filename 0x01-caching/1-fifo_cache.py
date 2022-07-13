f589241c004b931cbd0ea6d1c300563228c0ba6b
elayneh <betenatefera@gmail.com>
2022-07-13 12:17:24 +0300

    docs added

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
-""" FIFO """
+""" FIFO caching """
 from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    """ Class that inherits from BaseCaching """
+    """ Class that inherits from BaseCaching and is a caching system """
     def __init__(self):
         super().__init__()
         self.data = {}
         self.next_in, self.next_out = 0, 0
 
     def _pop(self):
-        """ Remove element """
+        """ FIFO algorithm, remove element """
         self.next_out += 1
         key = self.data[self.next_out]
         del self.data[self.next_out], self.cache_data[key]
 
     def _push(self, key, item):
-        """ Add element """
+        """ FIFO algorithm, add element """
         if len(self.cache_data) > BaseCaching.MAX_ITEMS - 1:
             print("DISCARD: {}".format(self.data[self.next_out + 1]))
             self._pop()
