f0f3206ca38e330eb2f0b6842175099be7c11580
elayneh <betenatefera@gmail.com>
2022-07-13 12:06:50 +0300

    docs added

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,30 +1,27 @@
-#!/bin/usr/env python3
+#!/usr/bin/env python3
 """ FIFO """
-from ast import Return
-from optparse import Values
 from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    """ Class to do with fifo ptinciple """
 
     def __init__(self):
+        """ Initiliaze """
         super().__init__()
 
     def put(self, key, item):
-        """ Assign item to the cache data """
-        if item and key:
-            ky = self.get(key)
-            if ky:
+        """ Assign item to  self.cache_data """
+        if key or item is not None:
+            valuecache = self.get(key)
+            if valuecache is None:
                 if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                    dellist = list(self.cache_data.keys())[0]
-                    del self.cache_data[dellist]
-                    print("DISCARD {}", dellist)
+                    keydel = list(self.cache_data.keys())[0]
+                    del self.cache_data[keydel]
+                    print("DISCARD: {}".format(keydel))
+
             self.cache_data[key] = item
 
-        def get(self, key):
-            """
-            Return:
-                the key Values
-            """
-            return self.cache_data.get(key)
+    def get(self, key):
+        """ Return value of the key """
+        returned = self.cache_data.get(key)
+        return returned
