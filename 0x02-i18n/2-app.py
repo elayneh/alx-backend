9f4eb8bede1bc775baec26c9161eabdd684c2615
elayneh <betenatefera@gmail.com>
2022-07-19 18:02:26 +0300

    task pushed

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+""" Get local """
+from flask import *
+from flask import Babel
+
+
+class config(object):
+    """ class to set up """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    BABEL_DEFAULT_LOCALE = 'en'
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """ return local """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """ Return html content"""
+    return render_template('2-index.html')
+
+
+if __name__ == "__main__":
+    app.run(port=4000)
