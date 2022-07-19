90aeb161e3dff74decd300bbe15ba938a5292763
elayneh <betenatefera@gmail.com>
2022-07-19 17:11:29 +0300

    first commit

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" Flask app """
+
+from math import remainder
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """ Returns the html file """
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run(port=4000)
